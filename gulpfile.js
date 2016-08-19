ba8e759728509fe3a3665e0f09c1fa8749526f5a
kasiveyser <kasiveyser@gmail.com>
2016-08-19 23:46:01 +0300

    rename dir "build" to "docs"

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,12 @@ var gulp = require('gulp'),
 	reload = browserSync.reload;
 
 var path = {
-	build: { //Тут мы укажем куда складывать готовые после сборки файлы
-		html: 'build/',
-		js: 'build/js/',
-		css: 'build/css/',
-		img: 'build/img/',
-		fonts: 'build/fonts/'
+	docs: { //Тут мы укажем куда складывать готовые после сборки файлы
+		html: 'docs/',
+		js: 'docs/js/',
+		css: 'docs/css/',
+		img: 'docs/img/',
+		fonts: 'docs/fonts/'
 	},
 	src: { //Пути откуда брать исходники
 		html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
@@ -36,12 +36,12 @@ var path = {
 		img: 'src/img/**/*.*',
 		fonts: 'src/fonts/**/*.*'
 	},
-	clean: './build'
+	clean: './docs'
 };
 
 var config = {
 	server: {
-		baseDir: "./build"
+		baseDir: "./docs"
 	},
 	tunnel: true,
 	host: 'localhost',
@@ -49,35 +49,35 @@ var config = {
 	logPrefix: "Frontend_Devil"
 };
 
-gulp.task('html:build', function () {
+gulp.task('html:docs', function () {
 	gulp.src(path.src.html) //Выберем файлы по нужному пути
 		.pipe(rigger()) //Прогоним через rigger
-		.pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+		.pipe(gulp.dest(path.docs.html)) //Выплюнем их в папку docs
 		.pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
 
-gulp.task('js:build', function () {
+gulp.task('js:docs', function () {
     gulp.src(path.src.js) //Найдем наш main файл
         .pipe(rigger()) //Прогоним через rigger
         .pipe(sourcemaps.init()) //Инициализируем sourcemap
         .pipe(uglify()) //Сожмем наш js
         .pipe(sourcemaps.write()) //Пропишем карты
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(gulp.dest(path.docs.js)) //Выплюнем готовый файл в docs
         .pipe(reload({stream: true})); //И перезагрузим сервер
 });
 
-gulp.task('style:build', function () {
+gulp.task('style:docs', function () {
     gulp.src(path.src.style) //Выберем наш main.scss
         .pipe(sourcemaps.init()) //То же самое что и с js
         .pipe(sass()) //Скомпилируем
         .pipe(prefixer()) //Добавим вендорные префиксы
         .pipe(cssmin()) //Сожмем
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(gulp.dest(path.docs.css)) //И в docs
         .pipe(reload({stream: true}));
 });
 
-gulp.task('image:build', function () {
+gulp.task('image:docs', function () {
     gulp.src(path.src.img) //Выберем наши картинки
         .pipe(imagemin({ //Сожмем их
             progressive: true,
@@ -85,38 +85,38 @@ gulp.task('image:build', function () {
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(gulp.dest(path.docs.img)) //И бросим в docs
         .pipe(reload({stream: true}));
 });
 
-gulp.task('fonts:build', function() {
+gulp.task('fonts:docs', function() {
     gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+        .pipe(gulp.dest(path.docs.fonts))
 });
 
-gulp.task('build', [
-    'html:build',
-    'js:build',
-    'style:build',
-    'fonts:build',
-    'image:build'
+gulp.task('docs', [
+    'html:docs',
+    'js:docs',
+    'style:docs',
+    'fonts:docs',
+    'image:docs'
 ]);
 
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
+        gulp.start('html:docs');
     });
     watch([path.watch.style], function(event, cb) {
-        gulp.start('style:build');
+        gulp.start('style:docs');
     });
     watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
+        gulp.start('js:docs');
     });
     watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
+        gulp.start('image:docs');
     });
     watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
+        gulp.start('fonts:docs');
     });
 });
 
@@ -128,4 +128,4 @@ gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
 
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('default', ['docs', 'webserver', 'watch']);
